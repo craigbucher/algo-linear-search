2ece8dc1102a051b7e3d7d2fe1bb912a5a313ef5
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 17:34:04 -0500

    added global search

--- python/linear_search.py
+++ python/linear_search.py
@@ -3,4 +3,18 @@ for number in range(1, 1001):
     array_to_search_through.append(number)
 
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
+    for i in range(0, len(array_to_search_through)):
+        if array_to_search_through[i] == value_to_find:
+            return i
+    return None
+
+def global_linear_search(value_to_find, array_to_search_through):
+    result = []
+    for i in range(0, len(array_to_search_through)):
+        if array_to_search_through[i] == value_to_find:
+            result.append(i)
+    return result
+
+# bananas_list = list('bananas')
+# #print(linear_search(20, array_to_search_through))
+# print(global_linear_search('a', bananas_list))
